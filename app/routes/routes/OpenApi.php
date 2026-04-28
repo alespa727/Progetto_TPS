4cf91e1d469f2f78c7187c32e27d1b0750c424fd
ale <spartano.alessio07@gmail.com>
2026-04-28 18:44:07 +0200

    dynamic hostname

--- app/routes/routes/OpenApi.php
+++ app/routes/routes/OpenApi.php
@@ -13,23 +13,22 @@ use Core\Router;
 use OpenApi\Attributes as OA;
 
 #[OA\Info("3.0.0", "idk", "my api")]
-#[OA\Server(url: "http://sketchpc.hopto.org")]
-#[OA\Server(url: "http://localhost")]
-#[OA\Server(url: "https://weightlessly-tres-dagmar.ngrok-free.dev")]
-#[OA\Server(url: "http://34.154.100.33")]
 #[Route(Method::Get, ["api", "openapi"], [], ContentTypes::Json)]
 class OpenApi extends Controller
 {
     function manageRequest(Request $request, Params $params): Response
     {
-        $openapi = (new \OpenApi\Generator())->generate([__DIR__.'/../']);
-       
+        $openapi = (new \OpenApi\Generator())->generate([__DIR__ . '/../']);
+        $openapi->servers = [
+            new \OpenApi\Annotations\Server([
+                'url' => 'http://' . $_SERVER['HTTP_HOST']
+            ])
+        ];
         $json = json_decode($openapi->toJson());
-        
-        $res=Response::new()
-                ->ok()
-                ->body($json);
+
+        $res = Response::new()
+            ->ok()
+            ->body($json);
         return $res;
     }
 }
- 
