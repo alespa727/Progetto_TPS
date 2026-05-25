62d9ec9c3ff6433d4b38721805662f6ac2d759dd
savvkey <savvkey@gmail.com>
2026-05-25 12:39:15 +0200

    create backup init.bat, remove populate.sql

--- init2.bat
+++ init2.bat
@@ -1,15 +1,6 @@
 @echo off
 cls
-cd C:/xampp/htdocs/app/routes/components
-
-echo Downloading query fixes.
-ping localhost -n 2 >nul
-cls
-echo Downloading query fixes.
-ping localhost -n 2 >nul
-
-curl -L -o C:/xampp/htdocs/GetComponents.php http://sketchpc.hopto.org/api/download/queryfix/1
-curl -L -o C:/xampp/htdocs/GetAllComponents.php http://sketchpc.hopto.org/api/download/queryfix/2
+cd C:/xampp/htdocs/
 
 echo Fetching database.
 ping localhost -n 2 >nul
@@ -20,7 +11,6 @@ ping localhost -n 2 >nul
 cd C:/xampp/htdocs/
 curl -L -o C:/xampp/htdocs/db.sql http://sketchpc.hopto.org/api/download/db.sql
 
-cls
 echo Creating database.
 ping localhost -n 2 >nul
 cls
